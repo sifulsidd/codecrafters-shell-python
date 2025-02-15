1faa7d564f1df54a28e429e18d3848ac6e600276
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-14 22:34:35 -0500

    test if exit 0 is ending program

--- app/main.py
+++ app/main.py
@@ -5,28 +5,26 @@ def main():
     # Uncomment this block to pass the first stage
     
     # I used a while statment which is basically an infinite loop but we can use a recursive algorithm
-    # while(True):
-    #     sys.stdout.write("$ ")
+    while(True):
+        sys.stdout.write("$ ")
 
-    # # Wait for user input
-    #     command = input()
-    #     if(len(command) == 0):
-    #         continue
-    #     print(f"{command}: command not found")
+    # Wait for user input
+        command = input()
+        if(command == "exit 0"):
+            break
+        if(len(command) == 0):
+            continue
+        print(f"{command}: command not found")
     
-    sys.stdout.write("$ ")
-    command = input()
+    # sys.stdout.write("$ ")
+    # command = input()
     
-    if(len(command) > 0):
-        if(command == "exit"):
-            print(" ",end=" 0")
-            print(" ")
-        else:
-            print(f"{command}: command not found")
     
+    # if(len(command) > 0):
+    #         print(f"{command}: command not found")
     
     # recursively call main within itself so it never breaks out, infinite loop
-    main()
+    # main()
     
     
 
