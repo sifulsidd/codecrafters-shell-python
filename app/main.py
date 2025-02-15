15068bd73925996fc2c118ca5e7170fafcf511c9
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-14 22:47:09 -0500

    test if echo is functioning

--- app/main.py
+++ app/main.py
@@ -5,16 +5,26 @@ def main():
     # Uncomment this block to pass the first stage
     
     # I used a while statment which is basically an infinite loop but we can use a recursive algorithm
-    while(True):
+    while True:
         sys.stdout.write("$ ")
-
     # Wait for user input
         command = input()
-        if(command == "exit 0"):
-            break
-        if(len(command) == 0):
+        parts = command.split(" ")
+        new_command = parts[0]
+        
+        if new_command == "echo":
+            sentence = ""
+            for i in range (1, len(parts)):
+                sentence += parts[i] + " "
+            print(sentence)    
+        elif(command == "exit 0"):
+            # break
+            # instead of breaking using command sys.exit(0), this indicates program has exited successfully
+            sys.exit(0)
+        elif(len(command) == 0):
             continue
-        print(f"{command}: command not found")
+        else:
+            print(f"{new_command}: command not found")
     
     # sys.stdout.write("$ ")
     # command = input()
@@ -26,6 +36,9 @@ def main():
     # recursively call main within itself so it never breaks out, infinite loop
     # main()
     
+    sentence = "hello"
+    parts = sentence.split(" ")
+    print(parts)
     
 
 
