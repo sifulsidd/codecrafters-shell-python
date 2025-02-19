c1be4e2cb8a5304fe8ab6bee75ad468ea582c19f
sifulsidd <sifulsiddiki@yahoo.com>
2025-02-19 17:04:38 -0500

    trying to run a program, trying to see if waiting and outputting works

--- app/main.py
+++ app/main.py
@@ -23,9 +23,14 @@ def main():
             # add all the params to the array, we will pass this in the next function
             for part in parts:
                 arr.append(part)
-            process = subprocess.Popen(arr, text= True)
-            print(process)
+            process = subprocess.Popen(arr,
+                                       stdout= subprocess.PIPE,
+                                       stderr= subprocess.PIPE, 
+                                       text= True)
             
+            process.wait()
+            output, error = process.communicate()
+            print(output.decode())
             
         
         else:    
